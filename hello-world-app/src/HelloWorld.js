e4be105a5ca3a9d7cece0e3398170062c93c9c08
Ted <ted@HM-tedmosoMBP.local>
2024-01-17 15:16:37 -0500

    quick push

---
+++ hello-world-app/src/HelloWorld.js
@@ -0,0 +1,14 @@
+function HelloWorld() {
+    return (
+        <div>
+            <h1>hello, world!</h1>
+            <ul>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+            </ul>
+        </div>
+    );
+}
+
+export default HelloWorld;
